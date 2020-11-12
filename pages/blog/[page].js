71b8d6249c119f40ab8fa5ae2d4a81cb2b3bb32a
meddlin <drushing.dev@gmail.com>
2020-11-12 00:01:18 -0600

    Basic paging functionality
    - index (landing page) now produces data to show paging buttons
    - new [page].js file to show a list of posts for a specific "page" number

---
+++ pages/blog/[page].js
@@ -0,0 +1,80 @@
+import Link from 'next/link';
+import utilStyles from '../../styles/utils.module.css';
+import { getSortedPostsData } from '../../lib/posts';
+import config from '../../blogConfig';
+
+/**
+ * Create the props data needed to display a list of posts for any particular page.
+ * @param {*} param
+ */
+export async function getStaticProps({ params }) {
+    const posts = getSortedPostsData();
+
+    const pageIndex = parseInt(params.page) - 1;
+    const startIndex = pageIndex * config.postsPerPage;
+    const endIndex = (pageIndex + 1) * config.postsPerPage;
+
+    const prevPosts = (pageIndex > 0) ? pageIndex : null;
+    const nextPosts = (endIndex >= posts.length) ? null : (pageIndex + 2);
+    const numPages  = (config.postsPerPage % getSortedPostsData().length) + 1;
+
+    return {
+        props: {
+            posts: posts.slice(startIndex, endIndex),
+            prevPosts,
+            nextPosts,
+            pageIndex,
+            numPages
+        }
+    }
+};
+
+/**
+ * Generate the static paths for pages based on how many posts we have
+ */
+export async function getStaticPaths() {
+    const numPages = (config.postsPerPage % getSortedPostsData().length) + 1;
+
+    return {
+        paths: [...Array(numPages)].map( (v, i) => {
+            return {
+                params: { page: (i + 1).toString() }
+            }
+        }),
+        fallback: false
+    }
+};
+
+const PostsPage = ({ posts, prevPosts, nextPosts }) => {
+    return (
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+            {posts.map( ({ id, date, title }) => (
+                <li className={utilStyles.listItem} key={id}>
+                    <Link href={`/posts/${id}`}>
+                        <a>{title}</a>
+                    </Link>
+                    <br />
+                    <small className={utilStyles.lightText}>
+                        <Date dateString={date} />
+                    </small>
+                </li>
+            ))}
+        </ul>
+
+        {prevPosts !== null && (
+            <Link href={"/blog/" + prevPosts} passHref>
+                <a>« see newer posts</a>
+            </Link>
+        )}
+        {nextPosts !== null && (
+            <Link href={"/blog/" + nextPosts} passHref>
+            <a>see older posts »</a>
+            </Link>
+        )}
+    </section>
+    );
+};
+
+export default PostsPage;
--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,30 @@ import Link from 'next/link';
 import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData, getSectionsData } from '../lib/posts';
+import config from '../blogConfig';
 
 export async function getStaticProps() {
 	const allPostsData = getSortedPostsData();
 	const allSectionsData = getSectionsData();
+
+	// Paging information
+	const startIndex = 0;
+	const endIndex = config.postsPerPage;
+	const prevPosts = null;
+	const nextPosts = (endIndex >= allPostsData.length) ? null : 2;
 	
 	return {
 		props: {
-			allPostsData,
-			allSectionsData
+			allPostsData: allPostsData.slice(startIndex, endIndex),
+			allSectionsData,
+			prevPosts,
+			nextPosts
 		}
 	}
 
 };
 
-export default function Home({ allPostsData, allSectionsData }) {
+export default function Home({ allPostsData, allSectionsData, prevPosts, nextPosts }) {
 	return (
 		<Layout home>
 			<Head>
@@ -63,6 +72,16 @@ export default function Home({ allPostsData, allSectionsData }) {
 							</li>
 						))}
 					</ul>
+					{prevPosts !== null && (
+						<Link href={"/blog/" + prevPosts} passHref>
+							<a>« see newer posts</a>
+						</Link>
+					)}
+					{nextPosts !== null && (
+						<Link href={"/blog/" + nextPosts} passHref>
+						<a>see older posts »</a>
+						</Link>
+					)}
 				</section>
 			</div>
 			
