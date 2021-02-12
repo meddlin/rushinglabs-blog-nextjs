b108a8736ba46e609ab033d062a9bc59211ac380
meddlin <drushing.dev@gmail.com>
2021-02-11 22:35:38 -0600

    Basic links, copied from Linktree

---
+++ pages/links.js
@@ -0,0 +1,33 @@
+function Links() {
+    return (
+        <div>
+            <h1>Links</h1>
+
+            <ul>
+                <li>
+                    <a href="https://www.youtube.com/channel/UCMbD6urmMQBUUxjjvyXgaxw">RushingLabs - YT</a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/channel/UC_y1S4qIDJ9G0CU2EzVeDtw">RushingLabs Gaming - YT</a>
+                </li>
+                <li>
+                    <a href="https://github.com/meddlin">GitHub</a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/rushinglabs/">Instagram</a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/meddlin_dev">Twitter</a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/Rushing-Labs-754569184895820">Facebook</a>
+                </li>
+                <li>
+                    <a href="https://kit.co/meddlin">Kit - Hardware I Use</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Links;
