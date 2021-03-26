840a0cc9d5168150af5a8e5ededb3f2afbefa785
meddlin <drushing.dev@gmail.com>
2021-03-25 22:36:12 -0500

    npm update - moved to React ^17.0.x

--- next.config.js
+++ next.config.js
@@ -2,3 +2,6 @@
  * By default Next.js will only pickup files with .js or .jsx extensions.
  * So, we need this config to extend that behavior.
  */
+ module.exports = {
+    /* config options here */
+  }
