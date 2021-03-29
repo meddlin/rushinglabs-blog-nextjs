c5d6407e2f3eabd835030b5dce6d1d55ccfc3f4a
meddlin <drushing.dev@gmail.com>
2021-03-28 19:36:06 -0500

    Simple about page "blurb"
    - also create a simple CSS module to keep the styles in order

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,23 @@
 import Layout from '../components/layout';
+import styles from '../styles/about.module.css';
 
 function About() {
     return (
         <Layout home>
-            <h1>About</h1>
-            <p>
-                This is the about page. "about" info goes here.
-            </p>
+            <div className={styles.mainContent}>
+                <p>
+                    I love programming and tinkering with computers, and I'm currently working as an
+                    AppSec Engineer. I've been employed in the tech industry since 2013, and have held 
+                    multiple software engineering positions interacting with various technology spanning 
+                    across COBOL, .NET/C#, JavaScript, and even a little Python sprinkled in.
+                </p>
+
+                <p>
+                    I blog about code and some of the ideas I run across in work and projects. I also
+                    post tutorials and tech videos to YouTube, and try to share as much of everything 
+                    on GitHub as I can.
+                </p>
+            </div>
         </Layout>
     );
 };
